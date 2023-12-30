152dcbdc35489374b7b2c384a262124ee1dc95c4
hypnosw <hypnoswu@gmail.com>
2023-12-29 21:33:40 -0800

    finishing up review

--- 02-backend/spring-boot-library/spring-boot-library/src/main/java/com/luv2code/springbootlibrary/dao/ReviewRepository.java
+++ 02-backend/spring-boot-library/spring-boot-library/src/main/java/com/luv2code/springbootlibrary/dao/ReviewRepository.java
@@ -8,6 +8,8 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
+
 public interface ReviewRepository extends JpaRepository<Review, Long> {
 
     Page<Review> findBookById(@RequestParam("book_id") Long bookId, Pageable pageable);
