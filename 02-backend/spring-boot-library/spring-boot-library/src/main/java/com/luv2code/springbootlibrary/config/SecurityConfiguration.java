be73ea9619cfa4f2eca8681b09c887f011db66e7
hypnosw <hypnoswu@gmail.com>
2024-01-05 18:39:49 -0800

    now has APIs that work with Stripe

--- 02-backend/spring-boot-library/spring-boot-library/src/main/java/com/luv2code/springbootlibrary/config/SecurityConfiguration.java
+++ 02-backend/spring-boot-library/spring-boot-library/src/main/java/com/luv2code/springbootlibrary/config/SecurityConfiguration.java
@@ -21,7 +21,8 @@ public class SecurityConfiguration {
         http.authorizeRequests(configurer -> configurer
                         .antMatchers("/api/books/secure/**",
                                 "/api/reviews/secure/**", "/api/messages/secure/**",
-                                "/api/admin/secure/**")
+                                "/api/admin/secure/**",
+                                "/api/payments/secure/**")
                 .authenticated())
                 .oauth2ResourceServer()
                 .jwt();
--- 02-backend/spring-boot-library/spring-boot-library/src/main/java/com/luv2code/springbootlibrary/service/BookService.java
+++ 02-backend/spring-boot-library/spring-boot-library/src/main/java/com/luv2code/springbootlibrary/service/BookService.java
@@ -4,14 +4,18 @@ package com.luv2code.springbootlibrary.service;
 import com.luv2code.springbootlibrary.dao.BookRepository;
 import com.luv2code.springbootlibrary.dao.CheckoutRepository;
 import com.luv2code.springbootlibrary.dao.HistoryRepository;
+import com.luv2code.springbootlibrary.dao.PaymentRepository;
 import com.luv2code.springbootlibrary.entity.Book;
 import com.luv2code.springbootlibrary.entity.Checkout;
 import com.luv2code.springbootlibrary.entity.History;
+import com.luv2code.springbootlibrary.entity.Payment;
 import com.luv2code.springbootlibrary.responsemodels.ShelfCurrentLoansResponse;
 
+import org.springframework.security.core.parameters.P;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.sql.Time;
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.util.ArrayList;
@@ -26,13 +30,15 @@ public class BookService {
     private BookRepository bookRepository;
     private CheckoutRepository checkoutRepository;
     private HistoryRepository historyRepository;
+    private PaymentRepository paymentRepository;
 
     //repository injection to set up the service
     public BookService(BookRepository bookRepository, CheckoutRepository checkoutRepository,
-                       HistoryRepository historyRepository){
+                       HistoryRepository historyRepository, PaymentRepository paymentRepository){
         this.bookRepository = bookRepository;
         this.checkoutRepository = checkoutRepository;
         this.historyRepository = historyRepository;
+        this.paymentRepository = paymentRepository;
     }
 
     // returns a book
@@ -46,6 +52,37 @@ public class BookService {
 
         if(!book.isPresent() || validateCheckout != null || book.get().getCopiesAvailable() <= 0){
             throw new Exception("Book doesn't exist or already checkecd out by user");
+        }
+
+        List<Checkout> currentBooksCheckedOut = checkoutRepository.findBooksByUserEmail(userEmail);
+
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+
+        boolean bookNeedsReturned = false;
+
+        for(Checkout checkout : currentBooksCheckedOut){
+            Date d1 = sdf.parse(checkout.getReturnDate());
+            Date d2 = sdf.parse(LocalDate.now().toString());
+
+            TimeUnit time = TimeUnit.DAYS;
+
+            double differenceInTime = time.convert(d1.getTime() - d2.getTime(), TimeUnit.MILLISECONDS);
+            if(differenceInTime < 0){
+                bookNeedsReturned = true;
+                break;
+            }
+        }
+
+        Payment userPayment = paymentRepository.findByUserEmail(userEmail);
+        if((userPayment != null && userPayment.getAmount() > 0) || (userPayment != null && bookNeedsReturned)){
+            throw new Exception("Outstanding fees");
+        }
+
+        if(userPayment == null){
+            Payment payment = new Payment();
+            payment.setAmount(00.00);
+            payment.setUserEmail(userEmail);
+            paymentRepository.save(payment);
         }
 
         // changing the number available
@@ -122,6 +159,22 @@ public class BookService {
         book.get().setCopiesAvailable(book.get().getCopiesAvailable() + 1);
 
         bookRepository.save(book.get());
+
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        Date d1 = sdf.parse(validateCheckout.getReturnDate());
+        Date d2 = sdf.parse(LocalDate.now().toString());
+
+        TimeUnit time = TimeUnit.DAYS;
+
+        double differenceInTime = time.convert(d1.getTime() - d2.getTime(), TimeUnit.MILLISECONDS);
+        if(differenceInTime < 0 ){
+            Payment payment = paymentRepository.findByUserEmail(userEmail);
+
+            payment.setAmount(payment.getAmount() + (differenceInTime * - 1));
+            paymentRepository.save(payment);
+        }
+
+
         checkoutRepository.deleteById(validateCheckout.getId());
 
         History history = new History(
